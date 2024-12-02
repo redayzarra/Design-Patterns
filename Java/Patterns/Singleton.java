8fe7ab0f23032569796b65cda582c54c70bf7a75
ReDay Zarra <redayzarra@gmail.com>
2024-12-02 03:50:09 -0500

    Adding comments to better understand singleton in Java

--- Java/Patterns/Singleton.java
+++ Java/Patterns/Singleton.java
@@ -2,17 +2,21 @@
 public class Singleton {
 	// Create a private variable for the current instance
 	private static Singleton instance;
+
+	// Create a private constant for thread safety lock
 	private static final Object lock = new Object();
 
+	// Create a private variable to store data
 	private String weatherData;
-
 	private Singleton() {
 		this.weatherData = "Sunny, 25°C";
 	}
 
+	// Create method to get the current instance or create first new one
 	public static Singleton getInstance() {
+		// Make sure the instance doesn't already exist, to create new one
 		if (instance == null) {
-			synchronized (lock) {
+			synchronized (lock) { // Use lock for thread safety
 				if (instance == null) {
 					instance = new Singleton();
 				}
@@ -21,14 +25,17 @@ public class Singleton {
 		return instance;
 	}
 
+	// Create method to get the current weather data
 	public String getWeatherData() {
 		return weatherData;
 	}
 
+	// Create method to set the current weather data
 	public void setWeatherData(String weatherData) {
 		this.weatherData = weatherData;
 	}
 	
+	// Test the design pattern
 	public static void main(String[] args) {
 		// Create the first instance of the singleton
 		Singleton weatherSystem = Singleton.getInstance();
