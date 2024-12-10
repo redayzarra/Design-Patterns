0397edae16db418ae8ae608716ca733be25e15ea
ReDay Zarra <redayzarra@gmail.com>
2024-12-09 21:53:26 -0500

    Fixing implementation of protoype design pattern

--- Java/Patterns/PrototypePatternDemo.java
+++ Java/Patterns/PrototypePatternDemo.java
@@ -1,7 +1,7 @@
 // Define the Prototype Interface
-interface Prototype {
+interface Prototype extends Cloneable {
     // Create an abstract method for cloning
-    Prototype clone(); 
+    Prototype clone() throws CloneNotSupportedException;
 }
 
 // Create a concrete class that implements the Prototype interface
@@ -25,26 +25,29 @@ class Document implements Prototype {
 
     // Implement the clone method
     @Override
-    public Document clone() {
-        // Create a new Document with the same content
-        return new Document(this.content); 
+    public Document clone() throws CloneNotSupportedException {
+        return (Document) super.clone(); // Call super.clone() to create a shallow copy
     }
 }
 
 // Test the Prototype Pattern
 public class PrototypePatternDemo {
     public static void main(String[] args) {
-        // Create an original document
-        Document originalDocument = new Document("This is the first version of the document.");
-        System.out.println("Original Document Content: " + originalDocument.getContent());
+        try {
+            // Create an original document
+            Document originalDocument = new Document("This is the first version of the document.");
+            System.out.println("Original Document Content: " + originalDocument.getContent());
 
-        // Clone the original document
-        Document clonedDocument = originalDocument.clone();
-        System.out.println("Cloned Document Content: " + clonedDocument.getContent());
+            // Clone the original document
+            Document clonedDocument = originalDocument.clone();
+            System.out.println("Cloned Document Content: " + clonedDocument.getContent());
 
-        // Modify the content of the original document
-        originalDocument.setContent("This is the second version of the document.");
-        System.out.println("Modified Original Document Content: " + originalDocument.getContent());
-        System.out.println("Cloned Document Content After Modification: " + clonedDocument.getContent());
+            // Modify the content of the original document
+            originalDocument.setContent("This is the second version of the document.");
+            System.out.println("Modified Original Document Content: " + originalDocument.getContent());
+            System.out.println("Cloned Document Content After Modification: " + clonedDocument.getContent());
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Can't slone this object.");
+        }
     }
 }
