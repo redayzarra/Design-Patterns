a9370a41622e19251b554d58c917356f682090c6
ReDay Zarra <redayzarra@gmail.com>
2024-12-15 23:27:55 -0500

    Creating SavingsAccount for bank question in Java

--- Java/Questions/BankAccount.java
+++ Java/Questions/BankAccount.java
@@ -1,4 +1,4 @@
-abstract class BankAccount {
+public abstract class BankAccount {
     private final String accountHolder;
     private final String accountType;
     private double balance; // Private attribute
@@ -40,5 +40,27 @@ abstract class BankAccount {
     // Getter for account holder
     public String getAccountHolder() {
         return this.accountHolder;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    // Constructor
+    public SavingsAccount(String accountHolder, double balance, double interestRate) {
+        super(accountHolder, "savings", balance);
+        this.interestRate = interestRate;
+    }
+
+    // Default constructor with interest rate of 5%
+    public SavingsAccount(String accountHolder, double balance) {
+        this(accountHolder, balance, 0.05);
+    }
+
+    // Apply interest
+    public void applyInterest() {
+        double newBalance = getBalance() + (getBalance() * this.interestRate);
+        // Using deposit to update balance
+        super.deposit(newBalance - getBalance());
     }
 }
