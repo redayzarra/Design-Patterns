fa2529e9d06b102984ae591705c11488d5cf08c9
ReDay Zarra <redayzarra@gmail.com>
2024-12-19 18:59:23 -0800

    Creating pizza attributes

---
+++ Java/Questions/Topping.java
@@ -0,0 +1,17 @@
+// Creating enums for pizza attributes
+enum Size {
+    SMALL, MEDIUM, LARGE
+}
+
+enum Crust {
+    THIN, THICK
+}
+
+enum Sauce {
+    TOMATO, BBQ
+}
+
+enum Topping {
+    JALAPENO, OLIVES, PINEAPPLE, MUSHROOMS
+}
+
